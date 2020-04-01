47cadb8652ec05b3c81c8d2d5f21c412aa9a90ec
Tony Wong <twong@pivotal.io>
2020-04-01 00:38:38 -0400

    Generate intermediate reports

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+tabula="$DIR/third_party/tabula/tabula-1.0.3-jar-with-dependencies.jar"
+
+scripts/generate_intermediate_reports.sh \
+  "$tabula" \
+  "$DIR/raw_reports" \
+  "$DIR/intermediate_reports"
+
