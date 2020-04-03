07d0f82e25f45f2b90b0c435d0441fba6a1dfc85
Tony Wong <twong@pivotal.io>
2020-04-03 15:54:27 -0400

    Fix growth factor calculation, added new cases field

--- scripts/generate_final_report.rb
+++ scripts/generate_final_report.rb
@@ -11,6 +11,7 @@ status_csv = File.join(raw_reports_dir, "covidtesting.csv")
 final_report_arr = []
 
 last_confirmed_positive = nil
+last_new_cases = nil
 
 CSV.parse(File.read(status_csv), headers: true).each do |row|
   date = row[0]
@@ -27,11 +28,20 @@ CSV.parse(File.read(status_csv), headers: true).each do |row|
   unless deaths.nil? || deaths.empty?
     final_report_entry['deaths'] = deaths.to_i
   end
-  if last_confirmed_positive != nil && confirmed_positive > 0
-    growth_factor = '%.2f' % confirmed_positive.fdiv(last_confirmed_positive)
+
+  new_cases = nil
+  if last_confirmed_positive != nil
+    new_cases = confirmed_positive - last_confirmed_positive
+    final_report_entry['new_cases'] = new_cases
+  end
+
+  if last_new_cases != nil && last_new_cases > 0
+    growth_factor = '%.2f' % new_cases.fdiv(last_new_cases)
     final_report_entry['growth_factor'] = growth_factor
   end
+
   last_confirmed_positive = confirmed_positive
+  last_new_cases = new_cases
 
   final_report_arr << final_report_entry
 end
