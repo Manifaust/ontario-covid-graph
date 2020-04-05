561231dce97fddf8a91a8ae95c479c50b7e2a25e
Garry Ing <garry.ing@gmail.com>
2020-04-05 13:23:16 -0400

    gh pages and charts

--- Gemfile
+++ Gemfile
@@ -7,3 +7,7 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem 'selenium-webdriver', '~> 3.0'
 gem 'webdrivers', '~> 4.0'
 
+
+gem "jekyll", "~> 4.0"
+
+gem "minima", "~> 2.5"
---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+const reportURL = '/report.json'
+
+const report = fetch(reportURL).then((response) => {
+  return response.json();
+}).then((data) => {
+  chartData.render(
+    document.getElementById('growthFactorChart'),
+    data,
+    'Growth Factor (Total Cases)',
+    'growth_factor_total_cases',
+    `255, 99, 132`
+  )
+
+  chartData.render(
+    document.getElementById('totalCases'),
+    data,
+    'Total Cases',
+    'total_cases',
+    `54, 162, 235`
+  )
+
+  chartData.render(
+    document.getElementById('newInfectedCases'),
+    data,
+    'New Infected Cases',
+    'new_infected_cases',
+    `255, 159, 64`
+  )
+})
+
+const chartData = {
+  render: (context, contextData, label, dataKey, rgb) => {
+    const chartLabels =  contextData.map(i => i.date)
+    const chartData = contextData.map(i => i[dataKey])
+    const myChart = new Chart(context, {
+        type: 'line',
+        data: {
+          labels: chartLabels,
+          datasets: [{
+              label: label,
+              data: chartData,
+              backgroundColor: [
+                `rgba(${rgb}, 0.2)`
+              ],
+              borderColor: [
+                `rgba(${rgb}, 1)`
+              ],
+              borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+            }],
+            xAxes: [{
+              type: 'time',
+              time: {
+                unit: 'day'
+              }
+            }]
+          }
+        }
+    });
+  }
+}
+
