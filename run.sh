347db737f7c282056cfec2c500199bb0c1c4217d
Tony Wong <twong@pivotal.io>
2020-04-10 23:40:03 -0400

    Download confirmed cases report

--- run.sh
+++ run.sh
@@ -7,15 +7,28 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 gem install bundler
 bundle install --jobs 4 --retry 3
 
+status_report_page_url='https://data.ontario.ca/dataset/status-of-covid-19-cases-in-ontario'
+status_report_csv_name='covidtesting.csv'
+status_report_destination="$DIR"/raw_reports/status-of-covid-19-cases-in-ontario.csv
+confirmed_cases_report_page_url='https://data.ontario.ca/dataset/confirmed-positive-cases-of-covid-19-in-ontario'
+confirmed_cases_report_csv_name='conposcovidloc.csv'
+confirmed_cases_report_destination="$DIR"/raw_reports/confirmed-positive-cases-of-covid-19-cases-in-ontario.csv
+
+function download_csv {
+  csv_url="$(scripts/fetch_status_report_url.rb $1 $2)"
+  echo "CSV URL: $csv_url"
+  status_csv_path=$3
+  curl "$csv_url" --output "$status_csv_path"
+  echo "Downloaded to: $status_csv_path"
+}
+
 echo 'Fetching CSV URL for: Status of COVID-19 cases in Ontario'
-csv_url="$(scripts/fetch_status_report_url.rb)"
-echo "Status CSV URL: $csv_url"
-status_csv_path="$DIR"/raw_reports/status-of-covid-19-cases-in-ontario.csv
-curl "$csv_url" \
-  --output "$status_csv_path"
-echo "Downloaded to: $status_csv_path"
+download_csv "$status_report_page_url" "$status_report_csv_name" "$status_report_destination"
+
+echo 'Fetching CSV URL for: Confirmed Cases of COVID-19 cases in Ontario'
+download_csv "$confirmed_cases_report_page_url" "$confirmed_cases_report_csv_name" "$confirmed_cases_report_destination"
 
 scripts/generate_final_report.rb \
-  "$status_csv_path" \
+  "$status_report_destination" \
   "$DIR/report.json"
 
--- scripts/fetch_status_report_url.rb
+++ scripts/fetch_status_report_url.rb
@@ -7,9 +7,13 @@ options = Selenium::WebDriver::Chrome::Options.new
 options.headless!
 driver = Selenium::WebDriver.for :chrome, options: options
 
+page_url = ARGV[0]
+csv_name = ARGV[1]
+
 begin
-  driver.get('https://data.ontario.ca/dataset/status-of-covid-19-cases-in-ontario')
-  download_button = driver.find_element(class: 'dataset-download-link')
+  driver.get(page_url)
+  download_button = driver.find_element(:xpath, "//a[contains(@href,'#{csv_name}')]")
+
   csv_url = download_button.attribute('href')
 
   puts csv_url
